6173bf3c4e5afc0e5312aeb8cef9c73bad19e82b
mikealberto <alberto.michael1701@gmail.com>
2022-09-20 18:34:59 -0400

    link css and js to html

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+/*----- constants -----*/
+const WORD_BANK = [
+  "encapsulation",
+  "inheritance",
+  "constructor",
+  "asynchronous",
+];
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+/*----- functions -----*/
