a7a9740275c01fbad28889e7413ad0ba80ff0239
mikealberto <alberto.michael1701@gmail.com>
2022-09-20 21:41:55 -0400

    Work on letter container and message display DOM elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("hello world");
-
 /*----- constants -----*/
 const WORD_BANK = [
   "encapsulation",
@@ -9,6 +7,22 @@ const WORD_BANK = [
 ];
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
+const messageDisplayEl = document.querySelector("h2");
+const letterContainerEl = document.getElementById("letter-container");
 
 /*----- event listeners -----*/
+letterContainerEl.addEventListener("click", handleLetterContainerClick);
+
 /*----- functions -----*/
+function init() {
+  messageDisplayEl.innerText =
+    "Welcome! Enter a letter to reveal the hidden word!";
+}
+
+function handleLetterContainerClick(evt) {
+  //   console.log("Say Hello");
+  console.log(evt);
+}
+
+//Game Initiated
+init();
