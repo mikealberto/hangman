4356cb4582abeae51720d2534b5d9543e5d2d849
mikealberto <alberto.michael1701@gmail.com>
2022-09-21 17:41:50 -0400

    work on inputSlotDisplay for output of dash where letters will be inputed once guessed

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const WORD_BANK = [
   "inheritance",
   "constructor",
   "asynchronous",
+  "class",
+  "compile",
 ];
 /*----- app's state (variables) -----*/
 let hiddenWord, correctLetters;
@@ -11,30 +13,28 @@ let hiddenWord, correctLetters;
 /*----- cached element references -----*/
 const messageDisplayEl = document.querySelector("h2");
 const letterContainerEl = document.getElementById("letter-container");
-const hiddenWordInputEl = document.getElementById("hidden-word-input");
-// console.log(hiddenWordInputEl);
+const guessInputSlotEl = document.getElementById("guess-input-slot");
 
 /*----- event listeners -----*/
 letterContainerEl.addEventListener("click", handleLetterContainerClick);
 
 /*----- functions -----*/
 function init() {
-  let hiddenWord = "";
-  let correctLetters = [];
-  console.log(correctLetters);
+  hiddenWord = "";
+  correctLetters = [];
   messageDisplayEl.innerText =
     "Welcome! Enter a letter to help reveal the hidden word!";
   hiddenWord = hiddenWordGenerator();
   console.log("init function ", hiddenWord);
-  //   hiddenWordDisplay();
+  inputSlotDisplay();
 }
 
 function handleLetterContainerClick(evt) {
   console.log(evt.target);
   const letterPlayerSelected = evt.target.innerText;
-  if (hiddenWord.includes(letterPlayerSelected)) {
-    // console.log("Right Letter");
-  }
+  //   if (hiddenWord.includes(letterPlayerSelected)) {
+  //     // console.log("Right Letter");
+  //   }
 }
 
 //Helper Function
@@ -44,15 +44,13 @@ function hiddenWordGenerator() {
   return WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].toUpperCase();
 }
 
-//display hidden word
+//display slot
 //Has to be done dynamically because we yet don't know the length of the word
-// function hiddenWordDisplay() {
-//   hiddenWordCharArr = hiddenWord.split("");
-//   console.log(hiddenWordCharArr);
-//   hiddenWordCharArr.map(function (character) {
-//     <span class="letter"></span>;
-//   });
-// }
+function inputSlotDisplay() {
+  for (let i = 0; i < hiddenWord.length; i++) {
+    guessInputSlotEl.innerHTML += `<span class="line-dash"> _ </span>`;
+  }
+}
 
 //Game Initiated
 init();
