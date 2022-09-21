6858881c52755a1962716b18948f36048ab81154
mikealberto <alberto.michael1701@gmail.com>
2022-09-21 11:59:23 -0400

    checkpoint for hidden-word-input and correctletter

--- js/main.js
+++ js/main.js
@@ -6,25 +6,35 @@ const WORD_BANK = [
   "asynchronous",
 ];
 /*----- app's state (variables) -----*/
-let hiddenWord = "";
+let hiddenWord, correctLetters;
 
 /*----- cached element references -----*/
 const messageDisplayEl = document.querySelector("h2");
 const letterContainerEl = document.getElementById("letter-container");
+const hiddenWordInputEl = document.getElementById("hidden-word-input");
+// console.log(hiddenWordInputEl);
 
 /*----- event listeners -----*/
 letterContainerEl.addEventListener("click", handleLetterContainerClick);
 
 /*----- functions -----*/
 function init() {
+  let hiddenWord = "";
+  let correctLetters = [];
+  console.log(correctLetters);
   messageDisplayEl.innerText =
     "Welcome! Enter a letter to help reveal the hidden word!";
   hiddenWord = hiddenWordGenerator();
-  console.log(hiddenWord);
+  console.log("init function ", hiddenWord);
+  //   hiddenWordDisplay();
 }
 
 function handleLetterContainerClick(evt) {
   console.log(evt.target);
+  const letterPlayerSelected = evt.target.innerText;
+  if (hiddenWord.includes(letterPlayerSelected)) {
+    // console.log("Right Letter");
+  }
 }
 
 //Helper Function
@@ -36,7 +46,13 @@ function hiddenWordGenerator() {
 
 //display hidden word
 //Has to be done dynamically because we yet don't know the length of the word
-function hiddenWordDisplay() {}
+// function hiddenWordDisplay() {
+//   hiddenWordCharArr = hiddenWord.split("");
+//   console.log(hiddenWordCharArr);
+//   hiddenWordCharArr.map(function (character) {
+//     <span class="letter"></span>;
+//   });
+// }
 
 //Game Initiated
 init();
