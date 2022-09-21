beb5c68197da1ff2aa52fbac5f86f4f5de271c96
mikealberto <alberto.michael1701@gmail.com>
2022-09-21 10:32:46 -0400

    finish hiddenWordGenerator function

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const WORD_BANK = [
   "asynchronous",
 ];
 /*----- app's state (variables) -----*/
+let hiddenWord = "";
+
 /*----- cached element references -----*/
 const messageDisplayEl = document.querySelector("h2");
 const letterContainerEl = document.getElementById("letter-container");
@@ -16,13 +18,25 @@ letterContainerEl.addEventListener("click", handleLetterContainerClick);
 /*----- functions -----*/
 function init() {
   messageDisplayEl.innerText =
-    "Welcome! Enter a letter to reveal the hidden word!";
+    "Welcome! Enter a letter to help reveal the hidden word!";
+  hiddenWord = hiddenWordGenerator();
+  console.log(hiddenWord);
 }
 
 function handleLetterContainerClick(evt) {
-  //   console.log("Say Hello");
-  console.log(evt);
+  console.log(evt.target);
 }
+
+//Helper Function
+
+//Elects Hidden Word for Game
+function hiddenWordGenerator() {
+  return WORD_BANK[Math.floor(Math.random() * WORD_BANK.length)].toUpperCase();
+}
+
+//display hidden word
+//Has to be done dynamically because we yet don't know the length of the word
+function hiddenWordDisplay() {}
 
 //Game Initiated
 init();
